af86ab7ea6d444c6bc1437cf67a9cf8e09921462
Israfil Hossen <sisrafilss@gmail.com>
2025-07-03 07:41:37 +0600

    project initialized

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-center mt-7 text-4xl text-green-600 font-semibold">
+        Welcome to Library Management System
+      </h1>
+    </>
+  );
+}
+
+export default App;
